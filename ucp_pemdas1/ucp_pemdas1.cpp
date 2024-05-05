c4c3c6fa058572b0ddc5008629e79eec20d3ef4b
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-05 17:33:21 +0700

    Add project files.

---
+++ ucp_pemdas1/ucp_pemdas1.cpp
@@ -0,0 +1,61 @@
+// int x = 5;
+// float
+// sring
+
+// prosedur dan fungsi
+//void sapa() {
+//cout << "Halo, selamat datang!" << endl;
+//}
+//int main memanggil
+
+// looping 
+// do
+// while
+// do while
+
+// struct 
+// int main() {
+ //  Mahasiswa mhs;
+    //cout << "Nomor Mahasiswa : ";
+    //cin >> mhs.nim;
+    //cout << "Nama Mahasiswa : ";
+    //cin >> mhs.nama;
+    //cout << "Alamat Mahasiswa : ";
+    //cin >> mhs.alamat;
+    //cout << "Umur Mahasiswa : ";
+    //cin >> mhs.umur;
+
+    //cout << endl;
+    //cout << "\n NIM : " << mhs.nim;
+    //cout << "\n Nama : " << mhs.nama;
+    //cout << "\n Alamat : " << mhs.alamat;
+    //cout << "\n Umur : " << mhs.umur;
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+char kota;
+char status;
+int nilai;
+
+int main() {
+    cout << "Masukan nama kota = ";
+    cin >> kota;
+    cout << "masukan nilai = " << endl;
+    cin >> nilai;
+    cout << "masukan status kota = ";
+    cin >> status;
+
+
+    cout << "\n kota :";
+    cout << "\n nilai = <=45:";
+    cout << "\n status = tidak sehat:";
+ 
+}
